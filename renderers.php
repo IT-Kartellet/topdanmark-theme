db1938d56a8d678c64fae949ad8bcd05a315295b
Christian Rasmussen <christian@it-kartellet.dk>
2014-09-16 09:56:58 +0200

    Added custom renderer for footer. Changed footer output in layout/columns*. Footer styling. Font-size in sectionname in course-content depends on screensize now, as well as margins on #page.

---
+++ renderers.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 9/16/14
+ * Time: 9:00 AM
+ */
+
+class theme_topdanmark_core_renderer extends theme_bootstrapbase_core_renderer{
+
+    public function topdanmark_info_footer(){
+        return '<footer id="page-footer">
+
+        <div id="address-info">
+            <p>Topdanmark A/S <br>
+               Borupvang 4 <br>
+               2750 Ballerup <br>
+               Cvr-nr: 78040017</p>
+        </div>
+
+    </footer>';
+    }
+
+}
